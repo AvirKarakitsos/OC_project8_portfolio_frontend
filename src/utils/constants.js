2b02bdefa42b383e90869891d89724b18b4f15b0
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-09-10 15:37:37 +0200

    fix: constantes 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const LOCAL_URL = 'http://localhost:4000';
-export const API_URL = 'https://arnocotsoyannis-api.onrender.com';
+export const API_URL = 'https://api-arnocotsoyannis.onrender.com';

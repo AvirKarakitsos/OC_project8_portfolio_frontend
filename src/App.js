c9147e08ffe005a9118eb746cce535d26b40d581
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-02-05 16:54:53 +0100

    fix: admin page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Admin from './pages/Admin'
 import Error from './pages/Error';
-// import RequireAuth from './config/RequireAuth';
+import RequireAuth from './config/RequireAuth';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { ThemeProvider } from './utils/context/ThemeContext';
@@ -17,9 +17,9 @@ function Router() {
 					<Routes>
 						<Route path="/" element={<Home/>} />
 						<Route path="/login" element={<Login/>} />
-						{/* <Route element={<RequireAuth/>}> */}
+						<Route element={<RequireAuth/>}>
 							<Route path="/admin" element={<Admin/>} />
-						{/* </Route> */}
+						</Route>
 						<Route path="/*" element={<Error/>} />
 					</Routes>
 				</LanguageProvider>
